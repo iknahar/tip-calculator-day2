200868ba81ab215eb7b365c99f6869f28f0d13c6
iknahar <knahar.kamrun@gmail.com>
2023-01-04 02:49:40 +0600

    instruction

--- tipcalculator.py
+++ tipcalculator.py
@@ -1,6 +1,6 @@
 total_bill = float(input("What was the total bill? $ "))
 people = int(input("How many people to split the bill? "))
-tipinpercent = float(input("What percent tip would you like to give? "))
+tipinpercent = float(input("What percent tip would you like to give? Do not add any % sign, add only number "))
 
 payable = (total_bill + ((tipinpercent*total_bill)/100))/people
 
